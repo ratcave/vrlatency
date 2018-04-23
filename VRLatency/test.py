9a42cf82f4b5c804648e892f6c61b0c66c7ae6cd
Mohammad Bashiri <mohammadbashiri93@gmail.com>
2018-04-24 01:35:40 +0200

    test code added

---
+++ VRLatency/test.py
@@ -0,0 +1,4 @@
+def test_function():
+	"Here is the sample explanation"
+	
+	print("Hey there.. You just successfullt ran your first python package!")
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='VRLatency',
+      version='0.1',
+      description='A library to measure and analyze the latency of virtual reality system',
+      url='https://github.com/mohammadbashiri93/VRLatency',
+      author='Mohammad Bashiri',
+      author_email='mohammadbashiri93@gmail.com',
+      license='MIT',
+      packages=['VRLatency'],
+      zip_safe=False)
