27546f9c1091181939b6078db7120ff22aa872a8
Mohammad Bashiri <mohammadbashiri93@gmail.com>
2018-04-24 11:14:42 +0200

    no changes

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
-setup(name='VRLatency',
-      version='0.1',
-      description='A library to measure and analyze the latency of virtual reality system',
-      url='https://github.com/mohammadbashiri93/VRLatency',
-      author='Mohammad Bashiri',
-      author_email='mohammadbashiri93@gmail.com',
-      license='MIT',
-      packages=['VRLatency'],
-      zip_safe=False)
+setup(
+	name='VRLatency',
+	version='0.1',
+	description='A library to measure and analyze the latency of virtual reality system',
+	license="MIT",
+	author='Mohammad Bashiri',
+	author_email='mohammadbashiri93@gmail.com',
+	url='https://github.com/mohammadbashiri93/VRLatency',
+	packages=find_packages(),
+)
