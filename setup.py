d5f8f57958769d80a7e654b9478d93d4b870e225
Nicholas A. Del Grosso <delgrosso@bio.lmu.de>
2018-04-25 14:47:25 +0200

    required packages for installation modifed

--- setup.py
+++ setup.py
@@ -1,23 +1,21 @@
 from setuptools import setup, find_packages
 
 setup(
-	name='VRLatency',
-	version='0.1',
-	description='A library to measure and analyze the latency of virtual reality system',
-	license="MIT",
-	author='Mohammad Bashiri',
-	author_email='mohammadbashiri93@gmail.com',
-	url='https://github.com/mohammadbashiri93/VRLatency',
-	packages=find_packages(),
-	
-	# required packages
-	install_required=[
-		'Click',
-	],
-	
-	entry_points=
-	'''
-		[console_scripts]
-		disp_latency_script = VRLatency.scripts.disp_latency_script:cli
-	''',
+    name='VRLatency',
+    version='0.1',
+    description='A library to measure and analyze the latency of virtual reality system',
+    license="MIT",
+    author='Mohammad Bashiri',
+    author_email='mohammadbashiri93@gmail.com',
+    url='https://github.com/mohammadbashiri93/VRLatency',
+    packages=find_packages(),
+
+    # required packages
+    install_requires=['numpy', 'matplotlib', 'seaborn', 'pandas', 'click', 'pyglet', 'ratcave'],
+
+    entry_points=
+    '''
+        [console_scripts]
+        disp_latency_script = VRLatency.scripts.disp_latency_script:cli
+    ''',
 )
