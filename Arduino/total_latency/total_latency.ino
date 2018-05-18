76855c9deb7a890871eb339b30b2febd0586d533
Mohammad Bashiri <mohammadbashiri93@gmail.com>
2018-05-18 16:15:40 +0200

    modified arduino code for the new LED configuration. testing experiments..

--- Arduino/total_latency/total_latency.ino
+++ Arduino/total_latency/total_latency.ino
@@ -1,9 +1,5 @@
-// Left LEDs: 11, 12, 13
-// Right LEDs: 8, 9, 10
-
-short right_LEDs[] = {8, 9, 10}; 
-short left_LEDs[] = {11, 12, 13}; 
-
+short right_LED = 12;
+short left_LED = 9;
 int analogPin_Left = 2;         // Left PhotoDiode connect on anaglog pin2  
 int analogPin_Right = 3;        // Right PhotoDiode connect on anaglog pin3  
 
@@ -23,14 +19,16 @@ struct Packet {
 void setup() {
   
   // initialize digital LED pin as an output.
-  for (int i=0; i<sizeof(right_LEDs); i++){
-    pinMode(right_LEDs[i], OUTPUT);
-    pinMode(left_LEDs[i], OUTPUT);
-    }
+  pinMode(10, OUTPUT);
+  pinMode(11, OUTPUT);
+  pinMode(right_LED, OUTPUT);
+  pinMode(left_LED, OUTPUT);
 
   // set the LEDs high or low
-  set_LEDs(left_LEDs, 3, LOW);
-  set_LEDs(right_LEDs, 3, HIGH);
+  digitalWrite(10, HIGH);
+  digitalWrite(11, HIGH);
+  digitalWrite(right_LED, HIGH);
+  digitalWrite(left_LED, LOW);
 
   // start seria comm
   Serial.begin(250000);       //  setup serial
@@ -38,29 +36,26 @@ void setup() {
   
 }
 
-void set_LEDs(short *array_of_LEDs, short array_size, bool level){
-  for (int i=0; i<array_size; i++){
-    digitalWrite(array_of_LEDs[i], level);
-    }
-  }
-
 void loop() {
   
   // switch LEDs and send timing data
   if (counter==delay_count){
+    
     trial++;
     counter = 1;
     delay_count = random(100, 300); // this keeps the minimum interval at 100 - necessary for window!
+    
     if (led_state){
-      set_LEDs(left_LEDs, 3, LOW);
-      set_LEDs(right_LEDs, 3, HIGH);
+      digitalWrite(right_LED, LOW);
+      digitalWrite(left_LED, HIGH);
       led_state = 0;
       }
     else{
-      set_LEDs(left_LEDs, 3, HIGH);
-      set_LEDs(right_LEDs, 3, LOW);
+      digitalWrite(right_LED, HIGH);
+      digitalWrite(left_LED, LOW);
       led_state = 1;
       }
+      
   }
     
   Packet data = {micros(), analogRead(analogPin_Left)/50, analogRead(analogPin_Right)/50, trial, led_state};
