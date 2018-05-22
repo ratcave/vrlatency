9d0cafd78abfb7aeedb7231c8a52db5030022c47
mohammadbashiri93 <mohammadbashiri93@gmail.com>
2018-05-22 14:44:05 +0200

    changed setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='VRLatency',
+    name='vrlatency',
     version='0.1',
     description='A library to measure and analyze the latency of virtual reality system',
     license="MIT",
