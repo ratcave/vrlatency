0a445f763684715c356f5676385adbbed460b37f
Mohammad Bashiri <mohammadbashiri93@gmail.com>
2018-05-22 12:04:00 +0200

    added two new arduino code for per frame data collection and rigid_body labelling

---
+++ Arduino/rigid_body/rigid_body.ino
@@ -0,0 +1,18 @@
+short right_LED = 12;
+short left_LED = 9;
+
+void setup() {
+  // initialize digital LED pin as an output.
+  pinMode(10, OUTPUT);
+  pinMode(11, OUTPUT);
+  pinMode(right_LED, OUTPUT);
+  
+  // set the LEDs high or low
+  digitalWrite(10, HIGH);
+  digitalWrite(11, HIGH);
+  digitalWrite(right_LED, HIGH);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
