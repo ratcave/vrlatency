f4ef4dd9e3959247b1ca161469f6581f9a408ace
Mohammad Bashiri <mohammadbashiri93@gmail.com>
2018-05-24 18:12:30 +0200

    it seems that when the overall sleep time is at .2 it goes crazy! checkout the size of the data buffer!

--- examples/displ_data_check.py
+++ examples/displ_data_check.py
@@ -1,19 +1,27 @@
 import vrlatency as vrl
+import numpy as np
+import matplotlib.pyplot as plt
 
 # specify and connect to device
 myarduino = vrl.Arduino(port='COM9', baudrate=250000, experiment_type='Display')
 
 # create a stimulus
 mystim = vrl.Stimulus(position=(0, 0), color=(1, 1, 1))
-mystim.mesh.point_size = 10
+mystim.mesh.point_size = 5
 mystim.mesh.scale.xyz = .03, .2, 1
 
 # create an experiment
 myexp = vrl.DisplayExperiment(arduino=myarduino,
-                              trials=100,
+                              trials=300,
                               fullscreen=True, screen_ind=1,
                               stim=mystim,
-                              on_width=.2,
-                              off_width=[.1, .3])
+                              on_width=.05,
+                              off_width=[0, .3])
 
 myexp.run()
+
+# reshape the data
+dd = np.array(myexp.data.values).reshape(-1, 3)
+
+plt.plot(dd[:, 1])
+plt.show()
