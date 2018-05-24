fd7ba8fa3d43d7cffaadef07f5ebcbd737845048
Mohammad Bashiri <mohammadbashiri93@gmail.com>
2018-05-24 15:06:55 +0200

    working on the example: playing around with scale and point size

--- examples/displ_data_check.py
+++ examples/displ_data_check.py
@@ -4,14 +4,13 @@ import vrlatency as vrl
 myarduino = vrl.Arduino(port='COM9', baudrate=250000, experiment_type='Display')
 
 # create a stimulus
-mystim = vrl.Stimulus(position=(.8, 0), color=(1, 0, 0))
-mystim.mesh.scale.y = .2
-# mystim.mesh.scale.y = .2
-# mystim.mesh.scale.xyz = .2, .2, 1
+mystim = vrl.Stimulus(position=(0, 0), color=(1, 1, 1))
+mystim.mesh.point_size = 10
+mystim.mesh.scale.xyz = .03, .2, 1
 
 # create an experiment
 myexp = vrl.DisplayExperiment(arduino=myarduino,
-                              trials= 100,
+                              trials=100,
                               fullscreen=True, screen_ind=1,
                               stim=mystim,
                               on_width=.2,
