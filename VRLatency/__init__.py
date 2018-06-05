8386f95a2ad87532f6bacd2bfdcd1350fe0e1969
mohammadbashiri93 <mohammadbashiri93@gmail.com>
2018-06-05 19:36:58 +0200

    deleted duplicate directory

--- VRLatency/__init__.py
+++
@@ -1,6 +0,0 @@
-from __future__ import absolute_import
-
-from .experiment import DisplayExperiment, TrackingExperiment, TotalExperiment
-from .stimulus import Stimulus
-from .data import Data
-from .arduino import Arduino
