afcd1462ac469a1f50d58eda18c8b7a75dd11052
Mohammad Bashiri <mohammadbashiri93@gmail.com>
2018-06-06 00:57:06 +0200

    added documentation

--- vrlatency/experiment.py
+++ vrlatency/experiment.py
@@ -10,17 +10,32 @@ from .data import Data
 
 
 class BaseExperiment(pyglet.window.Window):
-    """ Experiment object integrates other components and let's use to run, record and store experiment data
+    """Experiment abstract base method
 
+    Attributes:
+        - arduino:
+        - bckgrnd_color:
+        - stim:
+        - data:
+        - trials:
+        - current_trial:
+        - on_width:
+        - off_width:
     """
+
     def __init__(self, arduino=None, screen_ind=0, trials=20, stim=None,
                  on_width=.5, off_width=.5, bckgrnd_color=(0, 0, 0), *args, **kwargs):
-        """ Initialize an experiment object
+        """Integrates other components and let's use to run, record and store experiment data
 
-        Args:
-                - trials: number of trials
-                - filename: name for the recorded data
-                - path: path for saving the recorded data (if not given it will be saved in current directory)
+        Arguments:
+            - arduino (Arduino obj, optional): Arduino object used for recording and/or movement simulation
+            - screen_ind (int, optional): Specifies the screen in which the window app will be displayes
+            - trials (int, optional): Number of trials
+            - stim (Stimulation obj, optional): Stimulation object to be displayed by the display/projector
+            - on_width (float, optional): Time period for the stimulus to be visible
+            - off_width (float, optional): Time period for the stimulus to be invisible
+            - bckgrnd_color (float, optional): Backgorund color of experiment window app
+            - *args, **kwargs: other arguments of pyglet Window class
         """
 
         platform = pyglet.window.get_platform()
@@ -42,11 +57,12 @@ class BaseExperiment(pyglet.window.Window):
         self.off_width = _gen_iter(off_width)
 
     def end(self):
-        self.close()  # exit the pyglet app
+        """Ends the experiment by closing the app window and disconnecting arduino"""
+        self.close()
         self.arduino.disconnect() if self.arduino else None
 
     def run(self):
-        """ runs the experiment in the passed application window"""
+        """Runs the experiment"""
         for trial in range(1, self.trials + 1):
             self.dispatch_events()
             self.current_trial += 1
@@ -56,31 +72,36 @@ class BaseExperiment(pyglet.window.Window):
 
     @abstractmethod
     def run_trial(self):
+        """A single trial"""
         pass
 
     def on_key_press(self, symbol, modifiers):
+        """ Key press event for SPACE key"""
         if key.ESCAPE == symbol:
             self.end()
 
     @property
     def bckgrnd_color(self):
+        """Background color getter"""
         return self._bckgrnd_color
 
     @bckgrnd_color.setter
     def bckgrnd_color(self, value):
+        """Background color setter"""
         self._bckgrnd_color = value
         pyglet.gl.glClearColor(value[0], value[1], value[2], 1)
 
 
 class DisplayExperiment(BaseExperiment):
-    """ Experiment object to measure display latency measurement
+    """Measures display latency"""
 
-    """
     def __init__(self, stim, *args, **kwargs):
+        """ 
+        """
         super(self.__class__, self).__init__(*args, stim=stim, **kwargs)
 
     def run_trial(self):
-        """ a single trial"""
+        """A single trial"""
         self.clear()
         self.stim.draw()
         self.flip()
@@ -92,30 +113,32 @@ class DisplayExperiment(BaseExperiment):
 
 
 class TrackingExperiment(BaseExperiment):
-    """ Experiment object for tracking latency measurement
+    """Measures tracking latency
 
-    NOTE: in this experiment the timing information is recorded on Python side:
-    === Start of trial
-    1. Movement is sent to Arduino by python (communication time is insignificant)
-    2. LEDs move on the VRLatency shield and at the same time the LED positions are being tracked
-    3. Timing between the movement command and changes in position are compared and the tracking delay is characterize
-    === End of trial
+    NOTES: 
+        In this experiment the timing information is recorded on Python side. 
+        here is a description of a single trial:
+        1. Movement is sent to Arduino by python (communication time is insignificant)
+        2. LEDs move on the VRLatency shield and at the same time the LED positions are being tracked
+        3. Timing between the movement command and changes in position are compared and the tracking delay is characterize
     """
 
     def __init__(self, rigid_body, trial_period=.04, amplify_dist=-10, *args, **kwargs):
-        """ initialize a Tracking Experiment object
+        """ Integrates all the needed elements for tracking latency measuremnt
 
-        Args:
-                - rigid_body: is the object that has position attributes
+        Arguments:
+            - rigid_body (tracking obj): Tracking object that has position attributes
+            - trial_period (float, optional): Period of a single trial
+            - amplify_dist (float, optional): Amplification factor for stimulus distance, based on tracked object's position
+            - *args, **kwargs: other arguments accepted by BaseExperiment class
         """
         super(self.__class__, self).__init__(*args, visible=False, **kwargs)
-
         self.rigid_body = rigid_body
         self.trial_period = trial_period
         self.amplify_dist = amplify_dist
 
     def run_trial(self):
-        """ a single trial"""
+        """A single trial"""
         start_time = perf_counter()
         while (perf_counter() - start_time) < self.trial_period:
             t, led_pos = perf_counter(), self.amplify_dist * self.rigid_body.position.x
@@ -126,23 +149,30 @@ class TrackingExperiment(BaseExperiment):
 
 
 class TotalExperiment(BaseExperiment):
-    """ Experiment object for total latency measurement
+    """Experiment object for total latency measurement
 
-    NOTE: In this experiment the timing information is recorded by Arduino
-    === start of the trial
-    1. LED lights moves from one set of LEDs to another - timing info recorded
-    2. LED movement is tracked and a new images is displayed (on top of the corresponding sensor) -> timing info recorded
-    * the same process is repeated
-    === end of the trial
+    NOTES: 
+        In this experiment the timing information is recorded by Arduino
+        Here is a description of a single trial:
+        1. LED lights moves from one set of LEDs to another - timing info recorded
+        2. LED movement is tracked and a new images is displayed (on top of the corresponding sensor) -> timing info recorded
+        * the same process is repeated
     """
 
     def __init__(self, stim, rigid_body, *args, **kwargs):
-        super(self.__class__, self).__init__(*args, stim=stim, **kwargs)
+        """ Integrates all the needed elements for total latency measuremnt
 
+        Arguments:
+            - rigid_body (tracking obj): Tracking object that has position attributes
+            - stim (Stimulus obj, optional): Stimulation object to be displayed by the display/projector
+            - *args, **kwargs: other arguments accepted by BaseExperiment class
+        """
+
+        super(self.__class__, self).__init__(*args, stim=stim, **kwargs)
         self.rigid_body = rigid_body
 
     def run_trial(self):
-        """single trial"""
+        """A single trial"""
         self.stim.position = -self.rigid_body.position.x * .5, 0
         self.clear()
         self.stim.draw()
@@ -152,10 +182,11 @@ class TotalExperiment(BaseExperiment):
 
 
 def _gen_iter(vals):
+    """Generator function for on and off width given one or two values"""
     while True:
         if not hasattr(vals, '__iter__'):
-                yield vals
+            yield vals
         elif len(vals) == 2:
-                yield random.uniform(vals[0], vals[1])
+            yield random.uniform(vals[0], vals[1])
         else:
             raise TypeError("values passed as on_width and off_width must contain one or two values")
