1afb4583bfa4ab332b00807b39ad27b2010db9e4
mohammadbashiri93 <mohammadbashiri93@gmail.com>
2018-07-16 15:16:42 +0200

    testing experiment data, display latency

--- examples/displ_data_check.py
+++ examples/displ_data_check.py
@@ -3,12 +3,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # specify and connect to device
-myarduino = vrl.Arduino(port='COM9', baudrate=250000, experiment_type='Display')
+# myarduino = vrl.Arduino(port='COM9', baudrate=250000, experiment_type='Display')
+
+myarduino = vrl.Arduino.from_experiment_type(experiment_type='Display', port='COM9', baudrate=250000)
 
 # create a stimulus
 mystim = vrl.Stimulus(position=(0, 0), color=(1, 1, 1))
 mystim.mesh.point_size = 5
-mystim.mesh.scale.xyz = .03, .2, 1
+mystim.mesh.scale.xyz = .0225, .2, 1
 
 # create an experiment
 myexp = vrl.DisplayExperiment(arduino=myarduino,
@@ -22,3 +24,6 @@ myexp.run()
 
 # get the data
 dd = np.array(myexp.data.values).reshape(-1, 3)
+
+plt.plot(dd[:, 0]/1000000, dd[:, 1])
+plt.show()
