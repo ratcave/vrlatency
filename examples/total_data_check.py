056d248e5f1b9ff4390904bdcf11a453e7e6d9f4
mohammadbashiri93 <mohammadbashiri93@gmail.com>
2018-07-16 15:28:35 +0200

    reading data for tracking experiment, total experiment runs but stimulus does not move

--- examples/total_data_check.py
+++ examples/total_data_check.py
@@ -2,11 +2,11 @@ import vrlatency as vrl
 import natnetclient as natnet
 import numpy as np
 
-myarduino = vrl.Arduino.from_experiment_type(port='COM9', baudrate=250000, experiment_type='Total')
-
+# myarduino = vrl.Arduino.from_experiment_type(port='COM9', baudrate=250000, experiment_type='Total')
+myarduino = vrl.Arduino.from_experiment_type(experiment_type='Total', port='COM9', baudrate=250000)
 # create a stimulation pattern
 mystim = vrl.Stimulus(position=(0, 0), color=(1, 1, 1), size=5)
-mystim.mesh.scale.xyz = .03, .2, 1
+mystim.mesh.scale.xyz = .022, .2, 1
 
 # specify the object that is being tracked
 client = natnet.NatClient()
