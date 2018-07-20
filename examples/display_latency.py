21f6cd53347bacbcd1832c64334f3db77844c375
Mohammad Bashiri <mohammadbashiri93@gmail.com>
2018-07-20 17:40:44 +0200

    display exampe and tracking examples are checked and working fine

--- examples/display_latency.py
+++ examples/display_latency.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 myarduino = vrl.Arduino.from_experiment_type(experiment_type='Display', port='COM9', baudrate=250000)
 
 # create a stimulus
-mystim = vrl.Stimulus(position=(980, 510), size=5)
+mystim = vrl.Stimulus(position=(955, 447), size=5)
 
 # create an experiment
 myexp = vrl.DisplayExperiment(arduino=myarduino,
