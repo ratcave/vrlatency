05cfa67f61fed9cf57229751cb8c5c077ee02d2b
Mohammad Bashiri <mohammadbashiri93@gmail.com>
2018-08-02 16:51:09 +0200

    added the to_csv method, not tested yet

--- vrlatency/data.py
+++ vrlatency/data.py
@@ -11,11 +11,33 @@ class Data(object):
     def __init__(self):
         self.values = []
 
-    def to_csv(self, filename):
+    def _write_header():
+        raise NotImplementedError
+
+    def _write_data(file_obj, data, fieldnames):
+        writer = DictWriter(file_obj, delimiter=',', fieldnames=fieldnames)
+        writer.writeheader()
+
+        list_of_dicts = []
+        for values in data:
+            inner_dict = dict(zip(fieldnames, values))
+            list_of_dicts.append(inner_dict)
+
+        for dicts in list_of_dicts:
+            writer.writerow(dicts)
+
+
+    def to_csv(self, path, experiment_params=None, data_culomns=None):
         """ Save data into a csv file"""
-        with open(filename, 'w', newline='') as file:
-            writer = csv.writer(file, delimiter=' ')
-            writer.writerows(map(str, self.values))
+
+        # write the experiment parameters (header)
+        with open(path, "w"):
+            header = ['{}: {}\n'.format(key, value) for key, value in experiment_params.items()]
+            csv_file.writelines(header)
+
+            # check if data_culons are empty use integer indecies
+            self._write_data(csv_file, data, data_culomns)
+
 
     def analyze(self):
         raise NotImplementedError
