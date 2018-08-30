9a1815ccc9eb138a79d5455d0ed910330458318a
Mohammad Bashiri <mohammadbashiri93@gmail.com>
2018-08-30 11:30:49 +0200

    we have common options defined and added to subcommands in cli

--- vrlatency/cmd_api.py
+++ vrlatency/cmd_api.py
@@ -3,6 +3,7 @@ import vrlatency as vrl
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+
 def get_rigid_body(rigid_body):
     try:
         import natnetclient as natnet
@@ -19,42 +20,32 @@ def get_rigid_body(rigid_body):
     return led
 
 
+def add_options(options):
+    def _add_options(func):
+        for option in reversed(options):
+            func = option(func)
+        return func
+    return _add_options
+
+
+common_options = [
+    click.option('--port', default='COM9', help="Port that Arduino board is connected to"),
+    click.option('--baudrate', default=250000, help="Serial communication baudrate"),
+    click.option('--trials', default=20, help="Number of trials for measurement"),
+    click.option('--interval', default=.05, help="Time duration that a stimulus is shown in a trial, in seconds."),
+    click.option('--jitter/--no-jitter', default=True, help="Whether to add a randomized delay to the onset of stimulus presentation.")
+]
+
 
 @click.group()
 def cli():
     pass
 
-@cli.command()
-@click.option('--port', default='COM9', help="Port that Arduino board is connected to")
-@click.option('--baudrate', default=250000, help="Serial communication baudrate")
-@click.option('--trials', default=20, help="Number of trials for measurement")
-@click.option('--stimdistance', default=.01, help="Percent of screen width to move stimulus in Total experiment.")
-@click.option('--stimsize', default=10, help="Size of light stimulus projected onscreen.")
-@click.option('--screen', default=0, help="Monitor number to display stimulus on.")
-@click.option('--interval', default=.05, help="Time duration that a stimulus is shown in a trial, in seconds.")
-@click.option('--jitter/--no-jitter', default=True, help="Whether to add a randomized delay to the onset of stimulus presentation.")
-@click.option('--rigid_body', default='LED', help="Name of rigid body from tracker that represents the arduino's LEDs.")
-@click.option('--allmodes/--singlemode', default=False, help="Whether to run experiment repeatedly, for all screen modes.")
-def total(port, baudrate, trials, stimdistance, stimsize, screen, interval, jitter, rigid_body, allmodes):
-
-    led = get_rigid_body(rigid_body)
-
-    stim = vrl.Stimulus(size=stimsize)
-    arduino = vrl.Arduino.from_experiment_type(experiment_type='Total', port=port, baudrate=baudrate)
-    on_width = [interval, interval * 2] if jitter else interval
-    exp = vrl.TotalExperiment(arduino=arduino, trials=trials, fullscreen=True, on_width=on_width, screen_ind=screen, stim=stim, rigid_body=led, stim_distance=stimdistance)
-    exp.run()
-    exp.save()
-
 
 @cli.command()
-@click.option('--port', default='COM9', help="Port that Arduino board is connected to")
-@click.option('--baudrate', default=250000, help="Serial communication baudrate")
-@click.option('--trials', default=20, help="Number of trials for measurement")
+@add_options(common_options)
 @click.option('--stimsize', default=10, help="Size of light stimulus projected onscreen.")
 @click.option('--screen', default=0, help="Monitor number to display stimulus on.")
-@click.option('--interval', default=.05, help="Time duration that a stimulus is shown in a trial, in seconds.")
-@click.option('--jitter/--no-jitter', default=True, help="Whether to add a randomized delay to the onset of stimulus presentation.")
 @click.option('--allmodes/--singlemode', default=False, help="Whether to run experiment repeatedly, for all screen modes.")
 def display(port, baudrate, trials, stimsize, screen, interval, jitter, allmodes):
     arduino = vrl.Arduino.from_experiment_type(experiment_type='Display', port=port, baudrate=baudrate)
@@ -80,7 +71,6 @@ def display(port, baudrate, trials, stimsize, screen, interval, jitter, allmodes
         click.echo(df.head())
         latencies = vrl.get_display_latencies(df)
 
-
         sns.distplot(latencies.iloc[1:] / 1000.)
         plt.show()
 
@@ -89,11 +79,7 @@ def display(port, baudrate, trials, stimsize, screen, interval, jitter, allmodes
 
 
 @cli.command()
-@click.option('--port', default='COM9', help="Port that Arduino board is connected to")
-@click.option('--baudrate', default=250000, help="Serial communication baudrate")
-@click.option('--trials', default=20, help="Number of trials for measurement")
-@click.option('--interval', default=.05, help="Time duration that a stimulus is shown in a trial, in seconds.")
-@click.option('--jitter/--no-jitter', default=True, help="Whether to add a randomized delay to the onset of stimulus presentation.")
+@add_options(common_options)
 @click.option('--rigid_body', default='LED', help="Name of rigid body from tracker that represents the arduino's LEDs.")
 def tracking(port, baudrate, trials, interval, jitter, rigid_body):
 
@@ -102,6 +88,25 @@ def tracking(port, baudrate, trials, interval, jitter, rigid_body):
     arduino = vrl.Arduino.from_experiment_type(experiment_type='Tracking', port=port, baudrate=baudrate)
     on_width = [interval, interval * 2] if jitter else interval
     exp = vrl.TrackingExperiment(arduino=arduino, trials=trials, fullscreen=True, on_width=on_width, rigid_body=led)
+    exp.run()
+    exp.save()
+
+
+@cli.command()
+@add_options(common_options)
+@click.option('--stimdistance', default=.01, help="Percent of screen width to move stimulus in Total experiment.")
+@click.option('--stimsize', default=10, help="Size of light stimulus projected onscreen.")
+@click.option('--screen', default=0, help="Monitor number to display stimulus on.")
+@click.option('--rigid_body', default='LED', help="Name of rigid body from tracker that represents the arduino's LEDs.")
+@click.option('--allmodes/--singlemode', default=False, help="Whether to run experiment repeatedly, for all screen modes.")
+def total(port, baudrate, trials, stimdistance, stimsize, screen, interval, jitter, rigid_body, allmodes):
+
+    led = get_rigid_body(rigid_body)
+
+    stim = vrl.Stimulus(size=stimsize)
+    arduino = vrl.Arduino.from_experiment_type(experiment_type='Total', port=port, baudrate=baudrate)
+    on_width = [interval, interval * 2] if jitter else interval
+    exp = vrl.TotalExperiment(arduino=arduino, trials=trials, fullscreen=True, on_width=on_width, screen_ind=screen, stim=stim, rigid_body=led, stim_distance=stimdistance)
     exp.run()
     exp.save()
 
