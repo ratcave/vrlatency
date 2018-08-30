e1faa2cb61a9a892341b3c0da5026067607ce721
mohammadbashiri93 <mohammadbashiri93@gmail.com>
2018-08-30 11:37:01 +0200

    deleted commented code

--- vrlatency/analysis.py
+++ vrlatency/analysis.py
@@ -40,30 +40,8 @@ def get_tracking_latencies(df):
     return latencies
 
 
-# def get_total_latencies(df):
-#     """ Returns the latency values for each trial of a Total Experiment"""
-#
-#     def detect_latency(df, values, thresh):
-#         diff = np.diff(values > thresh)
-#         idx = np.where(diff != 0)[0][0]
-#         print('hi')
-#         return df.Time.iloc[idx] - df.Time.iloc[0]
-#
-#     sensor = ['LeftSensorBrightness', 'RightSensorBrightness']  # corresponding to: LED_State: False, True
-#     for i in range(len(df.groupby('LED_State'))):
-#         group = df.groupby('LED_State').get_group(i)
-#         latencies = group.groupby('Trial').apply(detect_latency, values=group[sensor[i]], thresh=group[sensor[i]].mean())
-#         print(latencies)
-#
-#     return latencies
-#
-#
-#
-
 def get_total_latencies(df):
     """ Returns the latency values for each trial of a Total Experiment"""
-    # import ipdb
-    # ipdb.set_trace()
 
     data = df.copy()
 
