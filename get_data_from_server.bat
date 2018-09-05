c4f4ed8af3a1db890592dd6860153edb05185b71
mohammadbashiri93 <mohammadbashiri93@gmail.com>
2018-09-05 20:24:28 +0200

    Added some more copy logic, updated notebook to get data over network.

--- get_data_from_server.bat
+++ get_data_from_server.bat
@@ -1,1 +1,10 @@
-.\vendor\pscp\pscp -pw %THETA_PASSWD% %THETA_USER%@%THETA_IP%:/storage/%THETA_USER%/vrlatency/*.csv ./data
+call set_secrets.bat
+
+@echo off
+set /p DELETE_ALL_DATA="Delete existing files (y/n): "
+IF %DELETE_ALL_DATA%==y (
+   call rmdir data /s /Q
+)
+
+call mkdir data
+call .\vendor\pscp\pscp -pw %THETA_PASSWD% %THETA_USER%@%THETA_IP%:/storage/%THETA_USER%/vrlatency/*.csv .\data
